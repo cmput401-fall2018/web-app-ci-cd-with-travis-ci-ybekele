a4f88a2b4ca291d026f24c6d0aa7717f49894ffb
yonael <yonael@ualberta.ca>
2018-10-03 09:03:52 -0600

    updating

---
+++ 401/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/yonaelbekele/Documents/GitHub/web-app-ci-cd-with-travis-ci-ybekele/401/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
