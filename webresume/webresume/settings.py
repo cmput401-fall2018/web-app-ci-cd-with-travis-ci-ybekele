821ce217253026fd543c9e43b6c162f51f8adb70
Ubuntu <yonael@ualberta.ca>
2018-10-18 21:18:20 +0000

    fix

--- webresume/webresume/settings.py
+++ webresume/webresume/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'EXCEPTION_HANDLER': 'rest_framework_json_api.exceptions.exception_handler',
