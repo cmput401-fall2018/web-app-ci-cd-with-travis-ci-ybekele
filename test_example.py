3bff1c2425b655955a683ace457c5479c4693517
Ubuntu <yonael@ualberta.ca>
2018-10-24 16:07:16 +0000

    test with selenium

--- test_example.py
+++ test_example.py
@@ -1,4 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 def test_tester():
-    if 1 == 1:
-        print("working")
+      driver = webdriver.Chrome()
+      driver.get("http://162.246.157.120:8000/")
+      elem = driver.find_element_by_id("name")
+      assert elem != None
     
