9a8f6339bd3248115623e66ef3f518dfe4c1bd15
yonael <yonael@ualberta.ca>
2018-12-07 14:02:05 -0700

    removed empty venv

--- 401/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/yonaelbekele/Documents/GitHub/web-app-ci-cd-with-travis-ci-ybekele/401/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
